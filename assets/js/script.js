d7584884e42c784f8b87877ef37d6e897e000afb
PickleRick <bmkersey@gmail.com>
2022-04-21 21:10:48 -0400

    refactored code and split function in two.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,26 +2,41 @@ var formE1 = document.querySelector("#task-form")
 var tasksToDoE1 = document.querySelector("#tasks-to-do")
 
 
-var createTaskHandler = function(event) {
+var taskFormHandler = function(event) {
 
     event.preventDefault();
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
-    
-//create list item
-    var listItemE1 = document.createElement("li");
-//create div to hold task info and add to list item
-    var taskInfoE1 = document.createElement("div");
-//give it a class name
-    taskInfoE1.className = "task-info"
-//add html content to div
-    taskInfoE1.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
-    listItemE1.appendChild(taskInfoE1);
 
-    listItemE1.className = "task-item";
+    //make data an object
+    var taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput
+    };
     
-    tasksToDoE1.appendChild(listItemE1);
+    createTaskE1(taskDataObj);
 
 };
 
-formE1.addEventListener("submit", createTaskHandler);
+
+var createTaskE1 = function(taskDataObj){
+
+    //create list item
+    var listItemE1 = document.createElement("li");
+    listItemE1.className = "task-item";
+    //create div to hold task info and add to list item
+    var taskInfoE1 = document.createElement("div");
+    //give it a class name
+    taskInfoE1.className = "task-info"
+    //add html content to div
+    taskInfoE1.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
+    listItemE1.appendChild(taskInfoE1);
+
+
+
+    tasksToDoE1.appendChild(listItemE1);
+
+
+}
+
+formE1.addEventListener("submit", taskFormHandler);
