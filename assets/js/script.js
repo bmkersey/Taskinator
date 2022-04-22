d085c975b51132892f98a0b0224d3c6bb1e8dd23
PickleRick <bmkersey@gmail.com>
2022-04-21 20:59:42 -0400

    modified js to grab input from users and create and display correctly

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,10 +5,21 @@ var tasksToDoE1 = document.querySelector("#tasks-to-do")
 var createTaskHandler = function(event) {
 
     event.preventDefault();
-
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    
+//create list item
     var listItemE1 = document.createElement("li");
+//create div to hold task info and add to list item
+    var taskInfoE1 = document.createElement("div");
+//give it a class name
+    taskInfoE1.className = "task-info"
+//add html content to div
+    taskInfoE1.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    listItemE1.appendChild(taskInfoE1);
+
     listItemE1.className = "task-item";
-    listItemE1.textContent = "This is a new task.";
+    
     tasksToDoE1.appendChild(listItemE1);
 
 };
